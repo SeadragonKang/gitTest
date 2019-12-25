4078860e450869a4db4524902f804b24bf126f07
lizhihao <lizhihao@iot-cas.com>
2019-12-25 09:19:30 +0800

    Test2

--- src/Test.java
+++ src/Test.java
@@ -8,5 +8,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("第一次上传");
 		System.out.println("二测");
+		System.out.println("三测");
 	}
 }
---
+++ src/Test2.java
@@ -0,0 +1,8 @@
+/**
+ * @author lizhihao@iot-cas.com
+ * @version V1.0
+ * @description: TODO
+ * @creat 2019-12-25-9:16
+ */
+public class Test2 {
+}
