1ad914b8db90e98d56930a7de784f3d362d7e534
lizhihao <lizhihao@iot-cas.com>
2019-12-25 09:00:43 +0800

    第一次提交

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author lizhihao@iot-cas.com
+ * @version V1.0
+ * @description: TODO
+ * @creat 2019-12-25-8:59
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("第一次上传");
+	}
+}
